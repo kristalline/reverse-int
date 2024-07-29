d7c0c194a20b6ffd3135ba00a232bf851d5f0737
evoly <svetoch.svs@gmail.com>
2024-07-29 21:23:27 +0700

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+    const iter = (val, acc) => {
+        if (val < 10) return `${acc}${val}`;
+
+        acc += `${val % 10}`;
+        return iter(Math.floor(val / 10), acc)
+    }
+
+    return iter(Math.abs(n), '');
 }
+
+
+const f = (n) => {
+    const val = Math.abs(n);
+
+    if (Math.abs(n) < 10) return `${val}`;
+
+    const result = val % 10;
+    const newVal = Math.floor(val / 10);
+
+    return `${result}${f(newVal)}`;
+}
+
